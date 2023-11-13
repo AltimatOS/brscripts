9ebbf0618556b6dc3c5ecaeacf360c6063b17076
Gary Greene <greeneg@tolharadys.net>
2023-11-12 21:48:35 -0500

    Initial build script for binutils
    Signed-off-by: Gary Greene <greeneg@tolharadys.net>

---
+++ cross-cc-tools/pkgs/binutils/build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+set -u
+set -o pipefail
+
+NAME=binutils
+VERSION=2.41
+SOURCE0="${NAME}-${VERSION}.tar.xz"
+SRC_DIR="${NAME}-${VERSION}"
+
+# get binutils first
+curl -O "https://ftp.gnu.org/gnu/binutils/$SOURCE0"
+
+# unpack it
+tar xvf "$SOURCE0"
+
+# prep
+pushd $SRC_DIR
+    mkdir -v build
+    pushd build
+        ../configure --prefix=/AltimatOS/tools --with-sysroot=/AltimatOS --target=$BLD_TARGET --disable-nls --enable-gprofng=no --disable-werror --enable-multilib
+        make
+        make install
+    popd
+popd
