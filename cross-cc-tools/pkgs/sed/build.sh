f6163260ac256d55b1be92e778942ee6bcea57a6
Gary Greene <greeneg@tolharadys.net>
2023-11-13 23:03:50 -0500

    Add GNU sed
    Signed-off-by: Gary Greene <greeneg@tolharadys.net>

---
+++ cross-cc-tools/pkgs/sed/build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+set -u
+set -o pipefail
+
+NAME=sed
+VERSION=4.9
+SOURCE0="${NAME}-${VERSION}.tar.xz"
+SRC_DIR="${NAME}-${VERSION}"
+
+# download
+curl -O https://ftp.gnu.org/gnu/${NAME}/${NAME}-${VERSION}.tar.xz
+
+# unpack
+tar xvf $SOURCE0
+
+pushd $SRC_DIR
+    ./configure --prefix=/System --host=$BLD_TARGET --build=$(./build-aux/config.guess)
+    make
+    make DESTDIR=$BLDROOT install
+popd
+
+# clean up
+rm *.xz
+rm -rf $SRC_DIR
